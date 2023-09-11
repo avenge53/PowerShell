f36c1ad54900b36d7ea34a127b53cd0f7fc38eea
avenge53 <christopher.johnson53@outlook.com>
2023-09-11 13:03:22 -0700

    user breakpoint

---
+++ project1.ps1
@@ -0,0 +1,18 @@
+Install-WindowsFeature -Name RSAT-AD-PowerShell
+
+Import-Module ActiveDirectory
+
+new-ADgroup 'Engineers' -Path 'OU=Managers, DC=adatum, DC=com' -GroupScope Global -GroupCategory Distribution
+
+$users = @(Alfie Power, Erin Bull, Sarah Burch)
+
+$groupname = 'Engineers' 
+
+foreach ($user in $users){
+    Add-ADGroupMember -Identity $groupname -Members $users
+}
+
+
+
+
+
