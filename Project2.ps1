cf3b57d1fcfd5cc4b50d9ededd485f1c710c7a0c
avenge53 <christopher.johnson53@outlook.com>
2023-09-14 10:59:58 -0700

    Software install progress

--- Project2.ps1
+++ Project2.ps1
@@ -2,71 +2,67 @@
 Import-Module ActiveDirectory
 
 # Identify objects #
-$availableWorkstations = @('LON-CL1','LON-SVR1', 'LON-DC1')
+$AvailableWorkstations = @('LON-CL1','LON-SVR1', 'LON-DC1')
 
 # Identify software package #
-$softwareName = "Git"
-$softwarepackage = $software
-
+$SoftwareName = "Git"
+$Software = 'E:\Git-2.42.0.2-64-bit.exe'
+$SoftwarePackage = $Software
 
 # Loop to add up to 10 users #
 for ($u=0; $u -lt 10; $u++)
 { 
         
     # Create user #
-    $username = Read-Host "Enter the username of the new employee" 
-    $securepassword = Read-Host "Enter the password of the new employee" -AsSecureString 
-    $Firstname = Read-Host "Enter the First name of the new employee"
-    $Lastname = Read-Host "Enter the Last name of the new employee"
+    $UserName = Read-Host "Enter the username of the new employee" 
+    $SecurePassword = Read-Host "Enter the password of the new employee" -AsSecureString 
+    $FirstName = Read-Host "Enter the First name of the new employee"
+    $LastName = Read-Host "Enter the Last name of the new employee"
     
-    New-ADUser -Name "$Firstname $Lastname" -GivenName $Firstname -Surname $Lastname -UserPrincipalName $username -SamAccountName $username -Enabled $true -AccountPassword $securepassword 
+    New-ADUser -Name "$FirstName $LastName" -GivenName $FirstName -Surname $LastName -UserPrincipalName $Username -SamAccountName $Username -Enabled $true -AccountPassword $SecurePassword 
    
-    #...rest of user input #
-    # Add user to group #
-    $groups = Read-Host "Enter the user group for new employee (comma-separated)"
-    $groupArray = $groups -split ","
-    $groupArray | Foreach-Object { Add-ADGroupMember -Identity $_.Trim() -Members $username}
-   
-    # Assign first available workstation #
-    $workstation = $availableWorkstations[0] 
-
-     
-        if ($availableWorkstations.Length -eq 0)
+    # Input additional user details and add user to group #
+    $Groups = Read-Host "Enter the user group for new employee (comma-separated)"
+    $GroupArray = $Groups -split ","
+    $GroupArray | Foreach-Object { Add-ADGroupMember -Identity $_.Trim() -Members $UserName}
+    
+    if ($AvailableWorkstations.Length -eq 0)
             {
             Write-Output "No more available workstations."
             break
             }
+    # Assign first available workstation #
+    $WorkStation = $AvailableWorkstations[0]     
     
-
     # Remove assigned workstation #
-    $availableWorkstations = $availableWorkstations[1..($availableWorkstations.Length - 1)] 
+    $AvailableWorkstations = $AvailableWorkstations[1..($AvailableWorkstations.Length - 1)] 
 
     # Install software on workstation #
-    $InstallerPath = 'E:\Git-2.42.0.2-64-bit.exe'
     
-    Foreach ($software in $softwarepackage) 
+    
+    Foreach ($Software in $SoftwarePackage) 
         {
-        Invoke-Command -ComputerName $workstation -ScriptBlock
+        Invoke-Command -ComputerName $WorkStation -ScriptBlock {Enable-PSRemoting}
             {
-        Start-Process -FilePath ${using}$InstallerPath -ArgumentList "/S" -Wait 
+        Start-Process -FilePath ${Using}$Software -ArgumentList "/S" -Wait 
             } 
         
-            Write-Output "Installing $softwareName on $workstation"
+            Write-Output "Installing $SoftwareName on $workStation"
         }
 
     # Verify software installation #    
-      $installedSoftware = Invoke-Command -ComputerName $workstation -ScriptBlock {}
+      $InstalledSoftware = Invoke-Command -ComputerName $WorkStation -ScriptBlock {
             
             Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName
-             
+            } 
             
         if ($installedSoftware.DisplayName -contains $softwareName) 
             {
-            Write-Output "$softwareName is installed on $workstation."
+            Write-Output "$SoftwareName is installed on $WorkStation."
             } 
       else  
             {
-            Write-Output "$softwareName is NOT installed on $workstation."
+            Write-Output "$SoftwareName is NOT installed on $WorkStation."
             }
 
        
